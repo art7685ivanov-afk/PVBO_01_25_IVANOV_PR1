a149f7a8608563789f4055b8f3f0cb2c8513697f
Ivanov Artem <art7685.ivanov@yandex.ru>
2026-09-22 10:01:39 +0300

    new pr

--- src/Main.java
+++ src/Main.java
@@ -105,20 +105,20 @@
 //}
 
 
-public class Main {
-
-    public static long factorial(int n) {
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
-    }
-
-    public static void main(String[] args) {
-        int number = 5;
-        long result = factorial(number);
-        System.out.println(number + "! = " + result);
-    }
-}
+//public class Main {
+//
+//    public static long factorial(int n) {
+//        long result = 1;
+//        for (int i = 1; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+//
+//    public static void main(String[] args) {
+//        int number = 5;
+//        long result = factorial(number);
+//        System.out.println(number + "! = " + result);
+//    }
+//}
 
